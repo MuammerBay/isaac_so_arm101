8ec158dd03e366e53ebc560d7874cd6b19a0579b
Dhyan Thakkar <thakk100@umn.edu>
2025-08-07 10:12:17 -0500

    Completely removed SKRL support

--- source/SO_100/SO_100/tasks/lift/__init__.py
+++ source/SO_100/SO_100/tasks/lift/__init__.py
@@ -18,7 +18,6 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.lift_env_cfg:SoArm100CubeCubeLiftEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:LiftCubePPORunnerCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
     disable_env_checker=True,
 )
@@ -29,7 +28,6 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.lift_env_cfg:SoArm100CubeCubeLiftEnvCfg_PLAY",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:LiftCubePPORunnerCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
     },
     disable_env_checker=True,
 )
--- source/SO_100/SO_100/tasks/reach/__init__.py
+++ source/SO_100/SO_100/tasks/reach/__init__.py
@@ -17,7 +17,6 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     kwargs={
         "env_cfg_entry_point": f"{__name__}.reach_env_cfg:SoArm100ReachEnvCfg",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:ReachPPORunnerCfg",
     },
     disable_env_checker=True,
@@ -28,7 +27,6 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     kwargs={
         "env_cfg_entry_point": f"{__name__}.reach_env_cfg:SoArm100ReachEnvCfg_PLAY",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:ReachPPORunnerCfg",
     },
     disable_env_checker=True,
