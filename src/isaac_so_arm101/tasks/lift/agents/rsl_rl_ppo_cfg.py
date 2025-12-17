be362225c1e3308012b4482c3f90b5adc2d1b820
vicwu <wudi@dexforce.com>
2025-12-17 09:13:33 +0800

    lift the cube with a gripper mounted camera

--- src/isaac_so_arm101/tasks/lift/agents/rsl_rl_ppo_cfg.py
+++ src/isaac_so_arm101/tasks/lift/agents/rsl_rl_ppo_cfg.py
@@ -43,3 +43,33 @@ class LiftCubePPORunnerCfg(RslRlOnPolicyRunnerCfg):
         desired_kl=0.01,
         max_grad_norm=1.0,
     )
+
+
+@configclass
+class CameraLiftCubePPORunnerCfg(RslRlOnPolicyRunnerCfg):
+    num_steps_per_env = 24
+    max_iterations = 4000
+    save_interval = 200
+    experiment_name = "camera_lift"
+    empirical_normalization = False
+    policy = RslRlPpoActorCriticCfg(
+        init_noise_std=1.0,
+        actor_hidden_dims=[256, 128, 64],
+        critic_hidden_dims=[256, 128, 64],
+        activation="elu",
+    )
+    algorithm = RslRlPpoAlgorithmCfg(
+        value_loss_coef=1.0,
+        use_clipped_value_loss=True,
+        clip_param=0.2,
+        entropy_coef=0.006,
+        num_learning_epochs=5,
+        num_mini_batches=4,
+        learning_rate=1.0e-6,
+        schedule="adaptive",
+        gamma=0.98,
+        lam=0.95,
+        desired_kl=0.01,
+        max_grad_norm=1.0,
+    )
+
